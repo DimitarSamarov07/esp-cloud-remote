1e10e16c39777bbe55700d4b331cb69242547546
Dimitar Samarov <samarovdimitar@gmail.com>
2024-11-07 10:04:33 +0200

    Implement basic websocket example that replays the message sent to the server

--- Back-End/server.js
+++ Back-End/server.js
@@ -3,25 +3,35 @@ import {fileURLToPath} from 'url';
 import {dirname} from 'path';
 import * as path from "node:path";
 
+import express_ws from "express-ws";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
+let expressWs = express_ws(app);
 
-let sendOptions = { root: "../Front-End"};
+let sendOptions = {root: "../Front-End"};
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, '/../Front-End/public/')));
 
+app.ws("/return-ws", function (ws, req) {
+    ws.on("message", function(msg){
+        ws.send(msg);
+    })
+})
+
+
 app.get('/', (req, res) => {
-    res.sendFile( 'index.html', sendOptions);
+    res.sendFile('index.html', sendOptions);
 })
 
 app.post('/double', (req, res) => {
     const number = req.body.number;
 
-    if(typeof number === 'number') {
-        const doubled = 2*number;
+    if (typeof number === 'number') {
+        const doubled = 2 * number;
         res.json({doubledNumber: doubled});
     } else {
         res.status(400).json({error: 'Please enter a number!'});
