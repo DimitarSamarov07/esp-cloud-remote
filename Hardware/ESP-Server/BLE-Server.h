4ce1af330865a3cd4babdfb1d86be4ac8b7f3b8f
Dimitar Samarov <samarovdimitar@gmail.com>
2025-01-21 13:24:54 +0200

    Working on combining files...still

---
+++ Hardware/ESP-Server/BLE-Server.h
@@ -0,0 +1,9 @@
+//
+// BLE-Server.h
+//
+
+// Setup code
+void ble_setup();
+
+// BLE main loop
+void ble_loop();
---
+++ Hardware/ESP-Server/ESP-Server.ino
@@ -0,0 +1,11 @@
+#include "BLE-Server.h"
+#include ""
+
+void setup() {
+  ble_setup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ble_loop();
+}
