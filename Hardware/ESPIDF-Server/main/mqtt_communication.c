bba94c71a81e7f9761c4d96200395bed1541fddd
AngelArabadzhiev08 <90851649+angelarabadzhiev@users.noreply.github.com>
2025-03-08 09:42:19 +0200

    Fixed issue with client persistence

--- Hardware/ESPIDF-Server/main/mqtt_communication.c
+++ Hardware/ESPIDF-Server/main/mqtt_communication.c
@@ -61,18 +61,18 @@ void mqtt_callback(const char* topic, const char* message, size_t length)
 
         if (strcmp(received_message, "TURN_LED_ON") == 0)
         {
-            esp_mqtt_client_publish(client, MQTT_AC_CONTROL_TOPIC, "AC turned ON", 0, MQTT_QOS, 0);
+            esp_mqtt_client_publish(client, MQTT_AC_CONTROL_TOPIC, "AC turned ON", 0, MQTT_QOS, 1);
             flash_led();
         }
         else if (strcmp(received_message, "TURN_LED_OFF") == 0)
         {
-            esp_mqtt_client_publish(client, MQTT_AC_CONTROL_TOPIC, "AC turned OFF", 0, MQTT_QOS, 0);
+            esp_mqtt_client_publish(client, MQTT_AC_CONTROL_TOPIC, "AC turned OFF", 0, MQTT_QOS, 1);
             flash_led();
         }
         else if (strcmp(received_message, "AC turned OFF") == 0 || strcmp(received_message, "AC turned ON") == 0 ||
             strcmp(received_message, "The AC is now OFF") == 0 || strcmp(received_message, "The AC is now ON") == 0)
-            {
-            }
+        {
+        }
         else
         {
             ESP_LOGW(TAG, "Unknown AC command: %s", received_message);
@@ -93,39 +93,26 @@ void mqtt_event_handler(void* handler_args, esp_event_base_t base, int32_t event
     switch (event->event_id)
     {
     case MQTT_EVENT_CONNECTED:
-
-        if (event->session_present)
-        {
-            ESP_LOGI(TAG, "MQTT Session Present");
-        }
-        else
-        {
-            ESP_LOGI(TAG, "MQTT Session Not present");
-            esp_mqtt_client_subscribe(client, MQTT_AC_CONTROL_TOPIC, MQTT_QOS);
-            esp_mqtt_client_subscribe(client, MQTT_WIFI_CONFIG_TOPIC, MQTT_QOS);
-        }
+        ESP_LOGI(TAG, "MQTT Connected!");
+        esp_mqtt_client_subscribe(client, MQTT_AC_CONTROL_TOPIC, MQTT_QOS);
+        esp_mqtt_client_subscribe(client, MQTT_WIFI_CONFIG_TOPIC, MQTT_QOS);
         break;
+
     case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGW(TAG, "MQTT Disconnected! Attempting to reconnect...");
+        vTaskDelay(8000 / portTICK_PERIOD_MS);
+        esp_mqtt_client_reconnect(client);
+        break;
 
+    case MQTT_EVENT_ERROR:
+        ESP_LOGE(TAG, "MQTT encountered an error");
         break;
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "Subscribed");
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "Unsubscribed");
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "Published");
-        break;
+
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "Received message on topic: %.*s", event->topic_len, event->topic);
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
         mqtt_callback(event->topic, event->data, event->data_len);
         break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT event error");
-        break;
+
     default:
         ESP_LOGI(TAG, "Unhandled MQTT event: %ld", event_id);
         break;
@@ -160,17 +147,20 @@ void wifi_init_sta()
 void mqtt_init()
 {
     vTaskDelay(3000 / portTICK_PERIOD_MS);
+
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker.address.uri = "mqtt://93.155.224.232:5728",
-        .session.last_will.msg = "Let me live    *dies*",
-        .session.keepalive = 4,
+        .session.keepalive = 20,
         .credentials.client_id = "mqtt_pc2",
-        .session.disable_clean_session = false,
+        .session.disable_clean_session = true,
         .session.disable_keepalive = false,
         .session.last_will.topic = MQTT_AC_CONTROL_TOPIC,
+        .session.last_will.msg = "ESP32 Disconnected",
+        .session.last_will.qos = 1,
+        .session.last_will.retain = true,
     };
+
     client = esp_mqtt_client_init(&mqtt_cfg);
     ESP_ERROR_CHECK(esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL));
     ESP_ERROR_CHECK(esp_mqtt_client_start(client));
-
 }
