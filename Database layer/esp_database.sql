06377cfea2df97280ed3ae8c3b3445eb35bd9176
AngelArabadzhiev08 <90851649+AngelArabadzhiev@users.noreply.github.com>
2025-07-25 09:48:11 +0300

    Added normalization to the scheme

--- Database layer/esp_database.sql
+++ Database layer/esp_database.sql
@@ -1,27 +1,28 @@
 CREATE DATABASE IF NOT EXISTS esp_remote;
-use esp_remote;
+USE esp_remote;
 
 CREATE TABLE Users (
-                       ID INT AUTO_INCREMENT PRIMARY KEY,
-                       Username VARCHAR(255) NOT NULL UNIQUE,
-                       Password VARCHAR(255) NOT NULL
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Username VARCHAR(255) NOT NULL UNIQUE,
+  Password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE Topics (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Topic VARCHAR(255) NOT NULL UNIQUE
+);
 
 CREATE TABLE ACL_Table (
-                           ID INT AUTO_INCREMENT PRIMARY KEY,
-                           UserID INT NOT NULL,
-                           Topic VARCHAR(255) NOT NULL,
-                           RW INT NOT NULL, -- 1 = read, 2 = write, 3 = read+write
-                           FOREIGN KEY (UserID) REFERENCES Users(ID)
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  UserID INT NOT NULL,
+  TopicID INT NOT NULL,
+  RW INT NOT NULL,
+  FOREIGN KEY (UserID) REFERENCES Users(ID),
+  FOREIGN KEY (TopicID) REFERENCES Topics(ID),
+  UNIQUE(UserID, TopicID)
 );
 
 CREATE TABLE Superusers (
-                            ID INT AUTO_INCREMENT PRIMARY KEY,
-                            Username VARCHAR(255) NOT NULL UNIQUE
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  Username VARCHAR(255) NOT NULL UNIQUE
 );
-
-
-
-
-
