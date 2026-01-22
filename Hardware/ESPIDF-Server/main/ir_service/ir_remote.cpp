0439762b5d5595ee2df61e707c6cf490c38a1742
AngelArabadzhiev <angelarabadzhiev82@gmail.com>
2026-01-22 17:15:27 +0200

    Removed `mqtt_communication` module; replaced direct calls with `mqtt_service` abstraction, updated headers, and refactored key workflows for consistency.

---
+++ Hardware/ESPIDF-Server/main/ir_service/ir_remote.cpp
@@ -0,0 +1,5 @@
+//
+// Created by angel on 22.01.26 г..
+//
+
+#include "ir_remote.h"
--- Hardware/ESPIDF-Server/main/wifi_service/wifi_control.h
+++ Hardware/ESPIDF-Server/main/wifi_service/wifi_control.h
@@ -4,8 +4,8 @@ extern "C" {
 #endif
 
     void wifi_init_setup();
-    void wifi_init_sta(char* ssid, char* password);
-    void change_wifi(char* new_ssid, char* new_password);
+    void wifi_init_sta(char* ssid, const char* password);
+    void change_wifi(char* new_ssid, const char* new_password);
 
     extern bool is_user_initiated_disconnect;
     extern int IS_WIFI_CONNECTED;
