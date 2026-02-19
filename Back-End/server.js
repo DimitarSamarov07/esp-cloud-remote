d6e7d6905d8905e87ea8fa0b3a696b7e4dacbfaa
AngelArabadzhiev08 <90851649+AngelArabadzhiev@users.noreply.github.com>
2026-02-19 17:31:38 +0200

    Remove console log of WiFi credentials
    Removed console log for sensitive data before changing WiFi.

--- Back-End/server.js
+++ Back-End/server.js
@@ -51,7 +51,6 @@ app.use("/public", express.static(path.join(__dirname, '/../Front-End/public/'))
 
 app.post("/changeWifi", async function (req, res) {
     let {deviceID, ssid, pass} = req.body;
-    console.log(deviceID, ssid, pass);
 
     await esp.changeWifi(deviceID, ssid, pass);
     res.sendStatus(200);
