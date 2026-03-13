d9574d6904b01e833ced5dba126d5024e4152af0
AngelArabadzhiev <angelarabadzhiev82@gmail.com>
2026-03-13 17:06:42 +0200

    Refactored MQTT AC control: replaced `ir_mitsubishi_control` with `ir_remote`, updated handler to use `sendTurnSignal`, and cleaned up `ir_remote.h` definitions.

--- Hardware/ESPIDF-Server/components/ir_service/include/ir_remote.h
+++ Hardware/ESPIDF-Server/components/ir_service/include/ir_remote.h
@@ -1,12 +1,12 @@
-// #define AC_CONTROL_H
-//
-//
-// #ifdef __cplusplus
-// #include <string>
-//
-// void startAcConnection();
-// void sendTurnSignal( bool state,  float temp,  std::string_view mode,  std::string_view fanSpeed,  bool swing);
-//
-//
-// #endif
-//
+#define AC_CONTROL_H
+
+
+#ifdef __cplusplus
+#include <string>
+
+void startAcConnection();
+void sendTurnSignal( bool state,  float temp,  std::string_view mode,  std::string_view fanSpeed,  bool swing);
+
+
+#endif
+
